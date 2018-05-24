77eff162d1aa14d26d7dc28778787364ea9aa593
bidetaggle <axel@archlinux.localdomain>
2018-05-25 01:45:33 +0300

    error fixed

--- api.php
+++ api.php
@@ -8,13 +8,15 @@ if(isset($_GET['from_timestamp']))
         $request = "SELECT * FROM btc_usd 
                     WHERE timestamp >= ? 
                     AND timestamp <= ?";
+        $db = Database::$dbh->prepare($request);
+        $db->execute(array($_GET['from_timestamp'], $_GET['to_timestamp']));
     }
     else{
         $request = "SELECT * FROM btc_usd 
                     WHERE timestamp >= ?";
+        $db = Database::$dbh->prepare($request);
+        $db->execute(array($_GET['from_timestamp']));
     }
-    $db = Database::$dbh->prepare($request);
-    $db->execute(array($_GET['from_timestamp'], $_GET['to_timestamp']));
 }
 else
 {
