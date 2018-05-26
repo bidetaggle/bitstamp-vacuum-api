3ce85968adb0cad659aea0c6d427b1701c280e30
bidetaggle <axel@archlinux.localdomain>
2018-05-26 20:54:39 +0300

    real time data

--- stats.php
+++ stats.php
@@ -13,93 +13,95 @@
 // based on prepared DOM, initialize echarts instance
 var myChart = echarts.init(document.getElementById('main'));
 
-$.get('https://luteciacorp.ovh/api.php?from_timestamp=1527090000&to_timestamp=1527200289').done(function(data){
-	data = JSON.parse(data);
-	console.log(data);
-	var x = data.map((value, index, array) => {
-		return value.id_local;
+setInterval(function(){
+	$.get('https://luteciacorp.ovh/api.php').done(function(data){
+		data = JSON.parse(data);
+
+		var x = data.reverse().map((value, index, array) => {
+			return value.id_local;
+		});
+		var y = data.reverse().map((value, index, array) => {
+			return value.price;
+		});
+
+		myChart.setOption({
+		    tooltip: {
+		        trigger: 'axis',
+		        position: function (pt) {
+		            return [pt[0], '10%'];
+		        }
+		    },
+		    title: {
+		        left: 'center',
+		        text: 'bitstamp vacuum USD/BTC',
+		    },
+		    toolbox: {
+		        feature: {
+		            dataZoom: {
+		                yAxisIndex: 'none'
+		            },
+		            restore: {},
+		            saveAsImage: {}
+		        }
+		    },
+		    xAxis: {
+		        type: 'category',
+		        boundaryGap: false,
+		        data: x
+		    },
+		    yAxis: {
+		        type: 'value',
+		        //axisLine: {onZero: 0},
+		        onZero: 0,
+		        boundaryGap: ['0%', '0%'],
+		        scale: true
+		    },
+		    dataZoom: [{
+		        type: 'inside',
+		        start: 50,
+		        end: 100
+		    }, {
+		        start: 0,
+		        end: 10,
+		        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+		        handleSize: '80%',
+		        handleStyle: {
+		            color: '#fff',
+		            shadowBlur: 3,
+		            shadowColor: 'rgba(0, 0, 0, 0.6)',
+		            shadowOffsetX: 2,
+		            shadowOffsetY: 2
+		        }
+		    }],
+		    series: [
+		        {
+		            name:'模拟数据',
+		            type:'line',
+		            smooth:true,
+		            symbol: 'none',
+		            sampling: 'average',
+		            itemStyle: {
+		                normal: {
+		                    color: 'rgb(255, 70, 131)'
+		                }
+		            },
+		            areaStyle: {
+		                normal: {
+		                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+		                        offset: 0,
+		                        color: 'rgb(255, 158, 68)'
+		                    }, {
+		                        offset: 1,
+		                        color: 'rgb(255, 70, 131)'
+		                    }])
+		                }
+		            },
+		            data: y
+		        }
+		    ]
+		});
 	});
-	var y = data.map((value, index, array) => {
-		return value.price;
-	});
-	console.log(x);
-	myChart.setOption({
-    tooltip: {
-        trigger: 'axis',
-        position: function (pt) {
-            return [pt[0], '10%'];
-        }
-    },
-    title: {
-        left: 'center',
-        text: '大数据量面积图',
-    },
-    toolbox: {
-        feature: {
-            dataZoom: {
-                yAxisIndex: 'none'
-            },
-            restore: {},
-            saveAsImage: {}
-        }
-    },
-    xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: x
-    },
-    yAxis: {
-        type: 'value',
-        //axisLine: {onZero: 0},
-        onZero: 0,
-        boundaryGap: ['0%', '0%'],
-        scale: true
-    },
-    dataZoom: [{
-        type: 'inside',
-        start: 0,
-        end: 100
-    }, {
-        start: 0,
-        end: 10,
-        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-        handleSize: '80%',
-        handleStyle: {
-            color: '#fff',
-            shadowBlur: 3,
-            shadowColor: 'rgba(0, 0, 0, 0.6)',
-            shadowOffsetX: 2,
-            shadowOffsetY: 2
-        }
-    }],
-    series: [
-        {
-            name:'模拟数据',
-            type:'line',
-            smooth:true,
-            symbol: 'none',
-            sampling: 'average',
-            itemStyle: {
-                normal: {
-                    color: 'rgb(255, 70, 131)'
-                }
-            },
-            areaStyle: {
-                normal: {
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                        offset: 0,
-                        color: 'rgb(255, 158, 68)'
-                    }, {
-                        offset: 1,
-                        color: 'rgb(255, 70, 131)'
-                    }])
-                }
-            },
-            data: y
-        }
-    ]
-});
-});
+}, 500);
 
 </script>
 
