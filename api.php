2a802025c44184993387a96c2af25ea1f6e6082e
bidetaggle <axel@archlinux.localdomain>
2018-05-27 17:59:12 +0300

    api gives all pairs by default

--- api.php
+++ api.php
@@ -2,36 +2,33 @@
 include_once('config.php');
 include_once('db.php');
 
+$requestValues = array();
+$request = "SELECT * FROM ".DB_TABLE_NAME." ";
 if(isset($_GET['pair']) && in_array($_GET['pair'], PAIRS_LIST)){
-    if(isset($_GET['from_timestamp']))
-    {
-        if(isset($_GET['to_timestamp'])){
-            $request = "SELECT * FROM ".DB_TABLE_NAME."
-                        WHERE pair = ?
-                        AND timestamp >= ?
-                        AND timestamp <= ?";
-            $db = Database::$dbh->prepare($request);
-            $db->execute(array($_GET['pair'], $_GET['from_timestamp'], $_GET['to_timestamp']));
-        }
-        else{
-            $request = "SELECT * FROM ".DB_TABLE_NAME."
-                        WHERE pair = ?
-                        AND timestamp >= ?";
-            $db = Database::$dbh->prepare($request);
-            $db->execute(array($_GET['pair'], $_GET['from_timestamp']));
-        }
-    }
-    else
-    {
-        $request = "SELECT * FROM ".DB_TABLE_NAME."
-                    WHERE pair = ?
-                    ORDER BY id_local DESC
-                    LIMIT 200";
-        $db = Database::$dbh->prepare($request);
-        $db->execute(array($_GET['pair']));
+    $request .= "WHERE pair = ?";
+    $requestValues[] = $_GET['pair'];
+}
+else $request .= "WHERE 1 ";
+
+if(isset($_GET['from_timestamp']))
+{
+    $request .= "AND timestamp >= ?";
+    $requestValues[] = $_GET['from_timestamp'];
+
+    if(isset($_GET['to_timestamp'])){
+        $request .= "AND timestamp <= ?";
+        $requestValues[] = $_GET['to_timestamp'];
     }
 
-    echo json_encode($db->fetchAll());
+    $db = Database::$dbh->prepare($request);
+    $db->execute($requestValues);
 }
 else
-    echo json_encode(array('error' => 'pair parameter missing or the provided pair is not valid'));
+{
+    $request .= "ORDER BY id_local DESC
+                LIMIT 200";
+    $db = Database::$dbh->prepare($request);
+    $db->execute($requestValues);
+}
+
+echo json_encode($db->fetchAll());
--- config.local.php
+++ config.local.php
@@ -4,5 +4,21 @@ define("DB_TABLE_NAME", "transactions");
 define("DB_USER", "root");
 define("DB_PASSWORD", "root");
 
-define("PAIRS_LIST", array("btc_usd", "eth_usd", "btc_eur"));
+define("PAIRS_LIST", array(
+    'btc_usd',
+	'btc_eur',
+	'eur_usd',
+	'xrp_usd',
+	'xrp_eur',
+	'xrp_btc',
+	'ltc_usd',
+	'ltc_eur',
+	'ltc_btc',
+	'eth_usd',
+	'eth_eur',
+	'eth_btc',
+	'bch_usd',
+	'bch_eur',
+	'bch_btc'
+));
 ?>
